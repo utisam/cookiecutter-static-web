2fbd35ad75a897d872a0c3bfb93f5aa08164f766
Masatoshi Tsushima <utisam@gmail.com>
2018-05-09 21:40:28 +0900

    Fix sourcemap

--- {{cookiecutter.project_name|replace(' ','-')|lower}}/php/lib/html5.php
+++ {{cookiecutter.project_name|replace(' ','-')|lower}}/php/lib/html5.php
@@ -5,10 +5,10 @@ function html5($title, $ext_head, $body)
 <!DOCTYPE html>
 <html lang="<?=LANGUAGE_CODE?>">
 <head>
-    <title><?=$title?></title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/base.css">
+  <title><?=$title?></title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/base.css">
 <?php $ext_head(); ?>
 </head>
 <body>
--- {{cookiecutter.project_name|replace(' ','-')|lower}}/php/src/index.php
+++ {{cookiecutter.project_name|replace(' ','-')|lower}}/php/src/index.php
@@ -4,23 +4,23 @@ require('lib/html5.php');
 
 html5(_('Title'), function () {
 ?>
-	<link rel="stylesheet" href="css/index.css">
+  <link rel="stylesheet" href="css/index.css">
 <?php
 }, function () {
 ?>
-	<main class="container">
-		<form id="sample-form" class="sample-form card">
-			<h2><?=_('Sample Form')?></h2>
-			<div class="sample-form-content">
-				<p>
-					<label for="message"><?=_('Message')?></label>
-					<input type="text" class="form-control message-input">
-				</p>
-				<button type="submit" class="btn submit-button"><?=_('Submit')?></button>
-			</div>
-		</form>
-		<section id="result-section"></section>
-	</main>
-	<script src="js/index.js"></script>
+  <main class="container">
+    <form id="sample-form" class="sample-form card">
+      <h2><?=_('Sample Form')?></h2>
+      <div class="sample-form-content">
+        <p>
+          <label for="message"><?=_('Message')?></label>
+          <input type="text" class="form-control message-input">
+        </p>
+        <button type="submit" class="btn submit-button"><?=_('Submit')?></button>
+      </div>
+    </form>
+    <section id="result-section"></section>
+  </main>
+  <script src="js/index.js"></script>
 <?php
 });
